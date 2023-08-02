cca95245d7c7f670617c2744782bd1475ef384c5
mohangowdak <mohan.k@testyantra.com>
2023-08-02 10:57:13 +0530

    first commit

---
+++ src/test/java/org/test/BasicTest.java
@@ -0,0 +1,21 @@
+package org.test;
+
+import org.testng.annotations.Test;
+
+public class BasicTest {
+
+    @Test
+    public void test1(){
+        System.out.println("test1");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("test2");
+    }
+
+    @Test
+    public void test3(){
+        System.out.println("test3");
+    }
+}
